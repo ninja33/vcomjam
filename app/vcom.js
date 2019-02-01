987ea66180bc32a09f07ad4172c1bbeb35290e66
ninja33 <zhenyu.huang@qq.com>
2019-02-01 17:42:03 +0800

    show wrong words by moving mouse over name

--- app/vcom.js
+++ app/vcom.js
@@ -1,25 +1,25 @@
 function renderMatchHTML(index, match) {
-    let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' };
+    let options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' };
     return `
         <div class="grouppanel">
             <div class="grouptitle">
-                <span>每日比赛结果 | Time: ${match.time.toLocaleDateString('zh-CN', options)} | Title: ${match.title}</span>
-                <span class="option-helps"><a href="#" target="_blank">…</a></span>
+                <span>${match.time.toLocaleDateString('zh-CN', options)} | ${match.title}</span>
+                <span class="extend"><a href="#">…</a></span>
             </div>
             <div class="leftpanel">
                 <div class="list-head">
-                    <span class="list-col list-col-name">Member</span><!--
-                    --><span class="list-col list-col-turn">1</span><!--
-                    --><span class="list-col list-col-turn">2</span><!--
-                    --><span class="list-col list-col-turn">3</span><!--
-                    --><span class="list-col list-col-turn">4</span><!--
-                    --><span class="list-col list-col-turn">5</span><!--
-                    --><span class="list-col list-col-turn">6</span><!--
-                    --><span class="list-col list-col-turn">7</span><!--
-                    --><span class="list-col list-col-turn">8</span><!--
-                    --><span class="list-col list-col-turn">9</span><!--
-                    --><span class="list-col list-col-turn">10</span><!--
-                    --><span class="list-col list-col-total">Total</span>
+                    <div class="list-col list-col-name">Member</div><!--
+                    --><div class="list-col list-col-turn">1</div><!--
+                    --><div class="list-col list-col-turn">2</div><!--
+                    --><div class="list-col list-col-turn">3</div><!--
+                    --><div class="list-col list-col-turn">4</div><!--
+                    --><div class="list-col list-col-turn">5</div><!--
+                    --><div class="list-col list-col-turn">6</div><!--
+                    --><div class="list-col list-col-turn">7</div><!--
+                    --><div class="list-col list-col-turn">8</div><!--
+                    --><div class="list-col list-col-turn">9</div><!--
+                    --><div class="list-col list-col-turn">10</div><!--
+                    --><div class="list-col list-col-total">Total</div>
             </div>
                 <div class="listwrapper">
                     <div id="dashboardbody-${index}" class="list-body"></div>
@@ -27,10 +27,10 @@ function renderMatchHTML(index, match) {
             </div>
             <div class="rightpanel">
                 <div class="list-head">
-                <span class="list-col list-col-pos">#</span><!--
-                --><span class="list-col list-col-word">Word</span><!--
-                --><span class="list-col list-col-pos">Pos.</span><!--
-                --><span class="list-col list-col-def">Def.</span>
+                <div class="list-col list-col-pos">#</div><!--
+                --><div class="list-col list-col-word">Word</div><!--
+                --><div class="list-col list-col-pos">Pos.</div><!--
+                --><div class="list-col list-col-def">Def.</div>
                 </div>
                 <div class="listwrapper">
                     <div id="wordlistbody-${index}" class="list-body"></div>
@@ -41,16 +41,13 @@ function renderMatchHTML(index, match) {
     `
 }
 
-
-
-
 function buildMatchs(results) {
     let matchs = [];
     for (const result of results) {
         let dashboard = {};
         let words = [];
         for (const member of result.members) {
-            dashboard[member.mid] = { nickname: member.nickname, totalpoints: member.points, turnpoints: [] };
+            dashboard[member.mid] = { nickname: member.nickname.replace("Anonymous","(A)"), totalpoints: member.points, turnpoints: [] };
         }
         for (const [index, turn] of result.turns.entries()) {
             words[index] = { word: turn.word.word, pos: turn.word.pos, def: turn.word.def };
@@ -70,11 +67,11 @@ function populateDashboard(index, dashboard) {
     sorted = Object.keys(dashboard).sort(function(x, y) { return dashboard[y].totalpoints - dashboard[x].totalpoints })
     for (memberid of sorted) {
         let row = '';
-        row += `<span class="list-col list-col-name">${dashboard[memberid].nickname}</span>`;
+        row += `<div class="list-col list-col-name">${dashboard[memberid].nickname}</div>`;
         for (const turnpoint of dashboard[memberid].turnpoints)
-            row += `<span class="list-col list-col-turn">${turnpoint}</span>`;
-        row += `<span class="list-col list-col-total">${dashboard[memberid].totalpoints}</span>`;
-        $('#dashboardbody-' + index).append($(`<div class="list-row">${row}</div>`));
+            row += `<div class="list-col list-col-turn">${turnpoint}</div>`;
+        row += `<div class="list-col list-col-total">${dashboard[memberid].totalpoints}</div>`;
+        $('#dashboardbody-' + index).append(`<div class="list-member-row" data-index="${index}" data-memberid="${memberid}">${row}</div><hr>`);
     };
 
 }
@@ -83,23 +80,46 @@ function populateWordlist(index, words) {
     $('#wordlistbody-' + index).empty();
     for ([idx, word] of words.entries()){
         let row = '';
-        row += `<span class="list-col list-col-pos">${idx+1}</span>`;
-        row += `<span class="list-col list-col-word">${word.word}</span>`;
-        row += `<span class="list-col list-col-pos">${word.pos}</span>`;
-        row += `<span class="list-col list-col-def">${word.def}</span>`;
-        $('#wordlistbody-' + index).append($(`<div class="list-row">${row}</div>`));
+        row += `<div class="list-col list-col-pos">${idx+1}</div>`;
+        row += `<div class="list-col list-col-word">${word.word}</div>`;
+        row += `<div class="list-col list-col-pos">${word.pos}</div>`;
+        row += `<div class="list-col list-col-def">${word.def}</div>`;
+        $('#wordlistbody-' + index).append(`<div id="m${index}-w${idx}" class="list-word-row">${row}</div><hr>`);
     };
+}
+
+function onMouseover(e){
+    if (!e.originalEvent) return;
+    $(e.currentTarget).toggleClass("highlight");
+    const ds = e.currentTarget.dataset;
+    const memberid = ds.memberid;
+    const index = ds.index;
+    points = matchs[index].dashboard[memberid].turnpoints;
+    for ([idx,point] of points.entries()){
+        if (point <= 0){
+            $(`#m${index}-w${idx} .list-col-word`).toggleClass("wrongword")
+        }
+    }
+}
+
+function onClickextend(e){
+    $(".container").toggleClass("width-600px");
+    $(".leftpanel").toggleClass("width-100pc");
+    $(".rightpanel").toggleClass("hidden");
 }
 
 function onReady() {
     $.getJSON("data/vcom.json", function(results) {
-        let matchs = buildMatchs(results);
-        for ([index, match] of matchs.reverse().entries()) {
+        matchs = buildMatchs(results).reverse();
+        for ([index, match] of matchs.entries()) {
             $('#jamresults').append(renderMatchHTML(index, match))
             populateDashboard(index, match.dashboard);
             populateWordlist(index, match.words);
         };
+        $(".list-member-row").hover(onMouseover);
+        $(".extend").click(onClickextend);
     });
 }
 
+var matchs = [];
 $(document).ready(onReady);
